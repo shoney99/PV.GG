7e7ed3baf6a87c294abc39fa1e343c5ee761b9fe
osh <gjs6589@naver.com>
2023-12-13 17:06:57 +0900

    ADD player ranking

--- rootWEB/mainApp/urls.py
+++ rootWEB/mainApp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from mainApp import views
 
 urlpatterns = [
-    path(''             , views.mainpage),
-    path('chart_inline/', views.chart_inline),
-    path('profile/'     , views.profile),
+    path(''              , views.mainpage),
+    path('chart_inline/' , views.chart_inline),
+    path('profile/'      , views.profile),
     path('widgets/'      , views.widgets),
-    # path('blank/', views.),
+    path('blank/'        , views.blank),
 ]
